1700c91dfa6c94b4af1dc34212d6ea8f517903f9
jeed123456789 <jongyup5020@gmail.com>
2019-06-19 12:09:49 +0900

    Editing lines

--- complica.py
+++ complica.py
@@ -6,6 +6,50 @@ import random
 # X = first player
 # O = computer
 
+def updateBoard(gameboard, row, col, piece):
+    gameboard[row][col] = piece
+    
+def isFilledUp(gameboard, col):
+    if gameboard[0][col] == ' ':
+        return False
+    else:
+        return True
+    
+def checkNextOpen(gameboard, FINAL_ROW, col):
+    for r in range(FINAL_ROW-1,-1,-1):
+        if gameboard[r][col] == ' ':
+            return r
+        
+def printBoard(gameboard, dec1, dec2, FINAL_ROW, FINAL_COL):
+    print(dec1)
+    for i in range(FINAL_ROW):
+        for j in range(FINAL_COL):
+            dec2= dec2+gameboard[i][j]+'|'
+        print(dec2)
+        print(dec1)
+        dec2 = '|'
+
+def checkWinner(gameboard,piece,FINAL_ROW,FINAL_COL):
+    # Check for horizontals
+    for i in range(FINAL_ROW):
+        for j in range(FINAL_COL-3):
+            if gameboard[i][j] == piece and gameboard[i][j+1] == piece and gameboard[i][j+2] == piece and gameboard[i][j+3] == piece:
+                return True
+    # Check for verticals
+    for i in range(FINAL_ROW-3):
+        for j in range(FINAL_COL):
+            if gameboard[i][j] == piece and gameboard[i+1][j] == piece and gameboard[i+2][j] == piece and gameboard[i+3][j] == piece:
+                return True
+    # Check for diagonals
+    for i in range(FINAL_ROW-3):
+        for j in range(FINAL_COL-3):
+            if gameboard[i][j] == piece and gameboard[i+1][j+1] == piece and gameboard[i+2][j+2] == piece and gameboard[i+3][j+3] == piece:
+                return True
+    for i in range(3,FINAL_ROW):
+        for j in range(FINAL_COL-3):
+            if gameboard[i][j] == 'X' and gameboard[i-1][j+1] == 'X' and gameboard[i-2][j+2] == 'X' and gameboard[i-3][j+3] == 'X':
+                return True
+            
 print('Hello, user! Welcome to playing the game of Complica!')
 print('Complia is a variant of Connect Four which once a Complica column is full, subsequent chips into that column push lowerlevel chips out; columns are essentially an infinite queue with limited space.')
 FINAL_ROW= int(input('State the numbers of rows you want in the gameboard: '))
@@ -31,123 +75,46 @@ while noWinner:
         else:
         # putting user input into the board
             # if the column is not filled up
-            if gameboard[0][col] == ' ':
-                for r in range(FINAL_ROW-1,-1,-1):
-                    if gameboard[r][col] == ' ':
-                        row = r
-                        break
-                print(row)
-                gameboard[row][col] = 'X'
-                print('filled user piece into board' )
-                print(gameboard)
+            if not isFilledUp(gameboard,col):
+                row = checkNextOpen(gameboard, FINAL_ROW, col)
+                updateBoard(gameboard,row,col,'X')
                 turn = 1
-                print(dec1)
-                for i in range(FINAL_ROW):
-                    for j in range(FINAL_COL):
-                        dec2= dec2+gameboard[i][j]+'|'
-                    print(dec2)
-                    print(dec1)
-                    dec2 = '|'
+                printBoard(gameboard,dec1,dec2,FINAL_ROW,FINAL_COL)
             # if the column is filled up 
             else: 
                 for r in range(FINAL_ROW-2,-1,-1):
                     gameboard[r+1][col] = gameboard[r][col]
-                gameboard[0][col] = 'X'
+                updateBoard(gameboard,0,col,'X')
+                printBoard(gameboard,dec1,dec2,FINAL_ROW,FINAL_COL)
                 turn = 1
-                print(dec1)
-                for i in range(FINAL_ROW):
-                    for j in range(FINAL_COL):
-                        dec2= dec2+gameboard[i][j]+'|'
-                    print(dec2)
-                    print(dec1)
-                    dec2 = '|'
     # Computer's turn
     else:
         comp_col = random.randint(0,FINAL_COL-1)
         # if the column is not filled up 
-        if gameboard[0][col] == ' ':
-            for r in range(FINAL_ROW-1,-1,-1):
-                if gameboard[FINAL_ROW-r-1][comp_col] == ' ':
-                    row = r
-            gameboard[row][comp_col]= 'O'
+        if isFilledUp(gameboard,comp_col):
+            row = checkNextOpen(gameboard, FINAL_ROW,comp_col)
+            updateBoard(gameboard,row,comp_col,'O')
             turn = 0
-            print(dec1)
-            for i in range(FINAL_ROW):
-                for j in range(FINAL_COL):
-                    dec2= dec2+gameboard[i][j]+'|'
-                print(dec2)
-                print(dec1)
-                dec2 = '|'
+            printBoard(gameboard,dec1,dec2,FINAL_ROW,FINAL_COL)
         # if the column is filled up
         else:
             for r in range(FINAL_ROW-2,-1,-1):
                 gameboard[r+1][comp_col] = gameboard[r][comp_col]
-            gameboard[0][comp_col] = 'O'
-            print(dec1)
-            for i in range(FINAL_ROW):
-                for j in range(FINAL_COL):
-                    dec2= dec2+gameboard[i][j]+'|'
-                print(dec2)
-                print(dec1)
-                dec2 = '|'
+            updateBoard(gameboard,0,comp_col,'O')
+            printBoard(gameboard,dec1,dec2,FINAL_ROW,FINAL_COL)
             turn = 0
     # Checking for winner(s)
-    # Check for horizontals
-    for i in range(FINAL_ROW):
-        for j in range(FINAL_COL-3):
-            if gameboard[i][j] == 'X' and gameboard[i][j+1] == 'X' and gameboard[i][j+2] == 'X' and gameboard[i][j+3] == 'X':
-                player_win = True
-                noWinner = False
-                break
-    for i in range(FINAL_ROW):
-        for j in range(FINAL_COL-3):
-            if gameboard[i][j] == 'O' and gameboard[i][j+1] == 'O' and gameboard[i][j+2] == 'O' and gameboard[i][j+3] == 'O':
-                comp_win = True
-                noWinner = False
-                break
-    # Check for verticals
-    for i in range(FINAL_ROW-3):
-        for j in range(FINAL_COL):
-            if gameboard[i][j] == 'X' and gameboard[i+1][j] == 'X' and gameboard[i+2][j] == 'X' and gameboard[i+3][j] == 'X':
-                player_win = True
-                noWinner = False
-                break
-    for i in range(FINAL_ROW-3):
-        for j in range(FINAL_COL):
-            if gameboard[i][j] == 'O' and gameboard[i+1][j] == 'O' and gameboard[i+2][j] == 'O' and gameboard[i+3][j] == 'O':
-                comp_win = True
-                noWinner = False
-                break
-    # Check for diagonals
-##    for i in range(FINAL_ROW-3):
-##        for j in range(FINAL_COL-3):
-##            if gameboard[i][j] == 'X' and gameboard[i+1][j+1] == 'X' and gameboard[i+2][j+2] == 'X' and gameboard[i+3][j+3] == 'X':
-##                player_win = True
-##                noWinner = False
-##                break
-##    for i in range(FINAL_ROW-3):
-##        for j in range(FINAL_COL-3):
-##            if gameboard[i][j] == 'O' and gameboard[i+1][j+1] == 'O' and gameboard[i+2][j+2] == 'O' and gameboard[i+3][j+3] == 'O':
-##                comp_win = True
-##                noWinner = False
-##                break
-##    for i in range(3,FINAL_ROW):
-##        for j in range(FINAL_COL-3):
-##            if gameboard[i][j] == 'X' and gameboard[i-1][j+1] == 'X' and gameboard[i-2][j+2] == 'X' and gameboard[i-3][j+3] == 'X':
-##                player_win = True
-##                noWinner = False
-##                break
-##    for i in range(3,FINAL_ROW):
-##       for j in range(FINAL_COL-3):
-##            if gameboard[i][j] == 'O' and gameboard[i-1][j+1] == 'O' and gameboard[i-2][j+2] == 'O' and gameboard[i-3][j+3] == 'O':
-##                comp_win = True
-##                noWinner = False
-##                break
+    if checkWinner(gameboard,'X',FINAL_ROW,FINAL_COL):
+        player_win = True
+        noWinner = False
+    if checkWinner(gameboard,'O',FINAL_ROW,FINAL_COL):
+        comp_win = True
+        noWinner = False
 #Printing the result of the game
 if player_win and comp_win:
     print('The game tied')
 elif player_win:
-    print('You win! Congrates!')
+    print('You win! Congrats!')
 elif comp_win:
     print('You lost! Try again!')
         
